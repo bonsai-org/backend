887896355e913cecf59d61dde668817231af2e48
DONTSTOPLOVINGMEBABY <henryjacobscode@gmail.com>
2024-07-18 20:35:12 -0700

    export application to be used in main function

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
+import EnvironmentVariables from './bin/environment-variables'
 import Express from 'express';
+import morgan from 'morgan';
 const app = Express();
-const PORT = process.env.PORT || 3000;
+
+if (EnvironmentVariables.NODE_ENV === 'dev') {
+  app.use(morgan('dev'))
+}
 
 app.get('*', (req, res) => {
   res.send('You have arrived at the node server');
 });
 
-export default app
+export default app;

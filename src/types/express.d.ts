3e08feca74f97f9231bf70947686301ab384832a
DONTSTOPLOVINGMEBABY <henryjacobscode@gmail.com>
2024-09-01 16:11:25 -0700

    extend request object with user property via declaration merging

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,11 +1,13 @@
 import * as express from 'express';
 import { SignUpRequest, LoginRequest } from './index';
+import { IUser } from './schemas';
 
 declare global {
   namespace Express {
     interface Request {
       signupRequest?: SignUpRequest;
       loginRequest?: LoginRequest;
+      user?: IUser
     }
   }
 }

0f8dfd01758aca5de67a6943b6ea96c22d2b1dfc
DONTSTOPLOVINGMEBABY <henryjacobscode@gmail.com>
2024-09-01 22:07:08 -0700

    add high level error handler to app object

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,17 @@
 /// <reference types="./types/environment.d.ts" />
 import Express from 'express';
 import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
 import userRouter from './routes/user';
 import router from './routes/bonsai';
+import { MongoError } from './errors/mongo-error';
+import { InternalApiError } from './errors/internalApiError';
+import { HttpStatusCode } from './types'
+import { sendAuthTokens } from './middleware/jwts'
 
 const app = Express();
+
+app.use(cookieParser());
 
 app.use(Express.json());
 if (process.env.NODE_ENV === 'dev') {
@@ -17,10 +24,23 @@ app.use('/api/user', userRouter);
 app.use('/api/bonsai', router);
 
 app.get('*', (req, res) => {
-  res.json({
-    message: 'Catchall: No endpoint exists for this path.',
-    statusCode: 404,
-  });
+  return res.sendStatus(HttpStatusCode.NotFound)
 });
+
+// Needs some kind of persistant logging! 
+
+app.use((err: unknown, req: Express.Request, res: Express.Response, next: Express.NextFunction) => {
+  if (
+    err instanceof MongoError ||
+    err instanceof InternalApiError
+  ) {
+    console.log(err.level)
+    console.log(err.message)
+    console.log(err.name)
+    err.stack ? console.log(err.level) : null
+  }
+  else { console.log(err) }
+  return res.sendStatus(HttpStatusCode.InternalServerError)
+})
 
 export default app;

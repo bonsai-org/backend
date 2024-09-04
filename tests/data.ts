5ee1ab8e5bbd1c2ba81ea1291c42b3c291a0f0d4
DONTSTOPLOVINGMEBABY <henryjacobscode@gmail.com>
2024-09-04 12:08:43 -0700

    create usable object with necessary data to test signup

--- tests/data.ts
+++ tests/data.ts
@@ -1,50 +1,76 @@
-type User = {
-  username: string;
-  password: string;
-  confirmPassword: string;
-  email: string;
-};
+import { IUser } from "../src/types/schemas";
 
-type UserRequests = {
-  GOOD_USER: User;
-  INVALID_EMAIL_USER: User;
-};
+type TestUser = {
+  username?: string,
+  password?: string,
+  confirmPassword?: string,
+  email?: string,
+}
 
-/*
- * A user with a valid email, password, confirm password, and username
- * fields
- */
+type UserFields = 'username' | 'password' | 'confirmPassword' | 'email'
 
-const GOOD_USER: User = {
-  username: 'MarcosHettinger',
-  password: 'TWVpeHfkH7GXXuC',
-  confirmPassword: 'TWVpeHfkH7GXXuC',
-  email: 'Lonnie50@yahoo.com',
-};
+export default class UserRequests {
 
-/*
- * A user with an invalid email with valid username, password, and
- * confirm password fields
- */
+  static removeSpecificField(user: TestUser, fieldName: UserFields): TestUser {
+    let copy = { ...user }
+    delete copy[fieldName]
+    return copy
+  }
 
-const INVALID_EMAIL_USER: User = {
-  username: 'MarcosHettinger',
-  password: 'TWVpeHfkH7GXXuC',
-  confirmPassword: 'TWVpeHfkH7GXXuC',
-  email: 'Lonnie50@yahoo',
-};
+  // A user that is successfully able to signup with bonsai org
+  static GOOD_USER: TestUser = {
+    username: 'MarcosHettinger',
+    password: 'TWVpeHfkH7GXXuC',
+    confirmPassword: 'TWVpeHfkH7GXXuC',
+    email: 'Lonnie50@yahoo.com',
+  }
 
-/*
- * A user with an invalid email
- *
- */
+  // A user with an invalid email
+  static INVALID_EMAIL_USER: TestUser = {
+    ...this.GOOD_USER,
+    email: 'Lonnie50@yahoo',
+  }
 
-/*
- *
- *
- */
+  // A user with the same username as good user but different email 
+  static SAME_USERNAME_DIFF_EMAIL_USER: TestUser = {
+    ...this.GOOD_USER,
+    email: 'German_Wisozk@yahoo.com',
+  }
 
-export const Users: UserRequests = {
-  GOOD_USER,
-  INVALID_EMAIL_USER,
-};
+  // A user with the same email as good user but different username 
+  static SAME_EMAIL_DIFF_USERNAME_USER: TestUser = {
+    ...this.GOOD_USER,
+    username: 'NealStiedemann',
+  }
+
+  // A user who only has a 6 character username instead of 8
+  static USERNAME_TOO_SHORT_USER: TestUser = {
+    ...this.GOOD_USER,
+    username: 'Marcos',
+  }
+
+  // A user whose username is 21 characters (max is 20)
+  static USERNAME_TOO_LONG_USER: TestUser = {
+    ...this.GOOD_USER,
+    username: 'GilesToyFisherAmbrose',
+  }
+
+  // A user whose password is 24 characters (max is 20)
+  static PASSWORD_TOO_LONG_USER = {
+    ...this.GOOD_USER,
+    password: 'passwordpasswordpassword',
+    confirmPassword: 'passwordpasswordpassword'
+  }
+
+  // A user who has a different password and confirm password field 
+  static DIFF_PASSWORD_CONFIRM_PASSWORD_USER: TestUser = {
+    ...this.GOOD_USER,
+    password: 'password',
+    confirmPassword: 'differentPassword'
+  }
+
+  static MISSING_USERNAME_FIELD = this.removeSpecificField(this.GOOD_USER, 'username')
+  static MISSING_PASSWORD_FIELD = this.removeSpecificField(this.GOOD_USER, 'password')
+  static MISSING_CONFIRM_PASSWORD_FIELD = this.removeSpecificField(this.GOOD_USER, 'confirmPassword')
+  static MISSING_EMAIL_FIELD = this.removeSpecificField(this.GOOD_USER, 'email')
+}

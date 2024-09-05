c47585f9fc0e39df3c510baf56ba182192c17a71
DONTSTOPLOVINGMEBABY <henryjacobscode@gmail.com>
2024-09-04 21:14:58 -0700

    add NODE_ENV as exportable variable

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,1 +1,4 @@
 export const __prod__ = (process.env.NODE_ENV === 'prod')
+export const __dev__ = (process.env.NODE_ENV === 'dev')
+export const __stage__ = (process.env.NODE_ENV === 'stage')
+export const __local__ = (process.env.NODE_ENV === 'local')

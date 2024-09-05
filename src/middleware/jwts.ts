ebd030b1dd5727ad5b882dc52392c189feb53217
DONTSTOPLOVINGMEBABY <henryjacobscode@gmail.com>
2024-09-05 10:16:09 -0700

    send fresh jwts to user if their accessToken expires

--- src/middleware/jwts.ts
+++ src/middleware/jwts.ts
@@ -119,7 +119,8 @@ function verifyRefreshToken(refreshToken: string): RefreshTokenData {
  * 3. If the refreshToken is valid, we will then have to make sure that the token's version matches the version
  *    that is saved on the user record. To do this, we have to query the user's record from our db with the username
  *    that is supplied via the JWT. If the two versions don't match, then we will have to send a 401.
- * 4. If all of these check out, we can then return the username and the user record.
+ * 4. If all of these check out, we will first send the user a new accessToken and fresh refreshToken
+ *    and then return the username and user record to the caller.
  *
  *  * NOTE!!! If we are able to verify an accessToken, checkTokens will only return a username.
  *  If we end up having to verify a refreshToken, then checkTokens will return the username
@@ -133,6 +134,7 @@ function verifyRefreshToken(refreshToken: string): RefreshTokenData {
 export async function checkTokens(
   accessToken: string,
   refreshToken: string,
+  res: Response,
 ): Promise<AuthorizedTokenData> {
   let accessTokenData = verifyAccessToken(accessToken);
   if (accessTokenData) {
@@ -157,6 +159,7 @@ export async function checkTokens(
       message: 'User has supplied an outdated refresh token',
     });
   }
+  sendAuthTokens(res, user)
   return {
     username: user.username,
     user,
@@ -171,7 +174,7 @@ export async function checkTokensMiddleware(
   try {
     debugger
     let { accessToken, refreshToken } = getCookieValues(req);
-    await checkTokens(accessToken, refreshToken);
+    await checkTokens(accessToken, refreshToken, res);
     req.loggedIn = true
     return next()
   } catch (error) {

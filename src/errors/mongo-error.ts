a88e36e0871e87d2d2bd1714881e15efda6b54da
DONTSTOPLOVINGMEBABY <henryjacobscode@gmail.com>
2024-10-01 15:21:17 -0700

    Add read failure + write failure messages to mongoerror

--- src/errors/mongo-error.ts
+++ src/errors/mongo-error.ts
@@ -1,6 +1,6 @@
 import { BaseError } from './baseError';
 
-type ErrorName = 'FAILED_TO_SAVE_NEW_USER' | 'FAILED_TO_LOOK_UP_EXISTING_USER';
+type ErrorName = 'FAILED_TO_SAVE_NEW_USER' | 'FAILED_TO_LOOK_UP_EXISTING_USER' | 'READ_FAILED' | 'WRITE_FAILED';
 
 export class MongoError extends BaseError<ErrorName> {
   constructor({
